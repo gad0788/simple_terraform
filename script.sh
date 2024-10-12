084f3836a247e50b12f0647ffe51a35d3fc14060
gad <nday.gady@gmail.com>
2024-10-12 16:32:34 -0700

    dev branch

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 sudo yum update -y
 sudo yum upgrade -y
 sudo yum install -y yum-utils
-sudo yum install -y git httpd
+sudo yum install -y git httpd wget
 sudo systemctl start httpd
 sudo systemctl enable httpd
