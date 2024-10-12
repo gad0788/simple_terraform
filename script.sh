eae792273bc648033402f5280ab9a1a97671d8ad
gad <nday.gady@gmail.com>
2024-10-12 16:39:41 -0700

    changed on script

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 sudo yum update -y
 sudo yum upgrade -y
 sudo yum install -y yum-utils
-sudo yum install -y git httpd wget vim finger
+sudo yum install -y git httpd wget vim finger curl
 sudo systemctl start httpd
 sudo systemctl enable httpd
